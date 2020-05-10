3d05feb97a9157769747e9ee03fe6bacc66a5cda
Vinícius Sales <vinicius.sbs@gmail.com>
2020-05-10 19:28:44 -0300

    Load author page's data from GraphQL API
    Carrega os dados da página do author da API GraphQL

--- app/routes/authors/author.js
+++ app/routes/authors/author.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
+import { queryManager } from 'ember-apollo-client';
+import AuthorQuery from 'ember-and-graphql/gql/queries/author.graphql';
 
 export default class AuthorsAuthorRoute extends Route {
+  @queryManager apollo
+
+  model({ id }) {
+    return this.apollo.watchQuery({ query: AuthorQuery, variables: { id } });
+  }
 }
