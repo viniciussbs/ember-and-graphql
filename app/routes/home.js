a56a4bbc2d1c443091bb44ee4efbf138ba8cdbab
Vinícius Sales <vinicius.sbs@gmail.com>
2020-05-10 18:04:49 -0300

    Load home page's data from GraphQL API
    Carrega os dados da página inicial da API GraphQL

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
+import { queryManager } from 'ember-apollo-client';
+import HomeQuery from 'ember-and-graphql/gql/queries/home.graphql';
 
 export default class HomeRoute extends Route {
+  @queryManager apollo
+
+  model() {
+    return this.apollo.watchQuery({ query: HomeQuery });
+  }
 }
