a291ae1d600a38af5f297233cbc03525f0054eb9
Vinícius Sales <vinicius.sbs@gmail.com>
2020-05-10 18:56:42 -0300

    Load post page's data from GraphQL API
    Carrega os dados da página da publicação da API GraphQL

--- app/routes/posts/post.js
+++ app/routes/posts/post.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
+import { queryManager } from 'ember-apollo-client';
+import PostQuery from 'ember-and-graphql/gql/queries/post.graphql';
 
 export default class PostsPostRoute extends Route {
+  @queryManager apollo
+
+  model({ id }) {
+    return this.apollo.watchQuery({ query: PostQuery, variables: { id } });
+  }
 }
